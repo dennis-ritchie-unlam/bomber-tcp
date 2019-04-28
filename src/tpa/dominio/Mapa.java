4f588c9bc77ed38e4258cccd9d2d8db488dbab6c
Matías Maximiliano Simone <matias.maximiliano.simone@gmail.com>
2019-04-28 20:01:50 -0300

    Implementación mapa

--- src/tpa/dominio/Mapa.java
+++ src/tpa/dominio/Mapa.java
@@ -2,53 +2,48 @@ package tpa.dominio;
 
 public class Mapa {
 	private Entidad[][] entidades;
-	
-	public Mapa(){
-		entidades = new Entidad[15][13];
+	static final int ALTO = 13;
+	static final int ANCHO = 15;
+
+	public Mapa() {
+		entidades = new Entidad[ALTO][ANCHO];
+		generarObstaculosIndestructibles();
+		generarObstaculosDestructibles();
 	}
-	
-	public void generarObstaculos() {
-		int i,j;	
-		//GENERA INDESTRUCTIBLES TODOS		
-		for(i=0; i<13; i++) {
-	        for(j=0; j<15; j++) {
-	            if(i==0 || i==12 || j==0 || j==14 ||(i%2==0 && j%2==0)) {
-	                entidades[i][j] = new Obstaculo(i, j, false);
-	            }
-	        }
-	    }
-		// GENERA OBSTACULOS DESTRUCTIBLES EJEMPLO
-		for(i=1; i<14; i++) {
-			entidades[i][4]= new Obstaculo( i, 4, true);
-		}
-		for(i=1; i<14; i++) {
-			entidades[i][10]= new Obstaculo( i, 10, true);
-		}
-		
-//		for(i=3; i<11; i++) 
-//			for(j=3; j<12; j+=2)
-//				entidad[i][4]= new Obstaculo( i, j, true);
+
+	private void generarObstaculosIndestructibles() {
+		for (int i = 0; i < ALTO; i++)
+			for (int j = 0; j < ANCHO; j++)
+				if (i == 0 || i == (ALTO - 1) || j == 0 || j == (ANCHO - 1) || (i % 2 == 0 && j % 2 == 0))
+					entidades[i][j] = new Obstaculo(i, j, false);
 	}
-	
-	public void eliminarObstaculo( int x, int y) {
-		if(entidades[x][y] instanceof Obstaculo && ((Obstaculo) entidades[x][y]).isDestructible() ) {
-			entidades[x][y]=null;
-		}
+
+	private void generarObstaculosDestructibles() {
+		for (int i = 1; i < (ANCHO - 1); i++)
+			entidades[i][4] = new Obstaculo(i, 4, true);
+		for (int i = 1; i < (ANCHO - 1); i++)
+			entidades[i][10] = new Obstaculo(i, 10, true);
 	}
-	
-	public Entidad[][] getEntidades(){	
+
+	public void eliminarObstaculo(int x, int y) {
+		if (entidades[x][y] instanceof Obstaculo && ((Obstaculo) entidades[x][y]).isDestructible())
+			entidades[x][y] = null;
+	}
+
+	public Entidad[][] getEntidades() {
 		return entidades;
 	}
-	
-	public void añadirBomba( Bomba bombita ) {
-		
+
+	public void añadirBomba(Bomba bombita) {
+		entidades[(int) bombita.getPosicionX()][(int) bombita.getPosicionY()] = bombita;
 	}
-	
-	public void eliminarBomba( int x, int y) {
-		
+
+	public void eliminarBomba(int x, int y) {
+		if (entidades[x][y] instanceof Bomba)
+			entidades[x][y] = null;
 	}
-	
-	public void añadirBomber( Bomber personaje) {
-		
+
+	public void añadirBomber(Bomber personaje) {
+		entidades[(int) personaje.getPosicionX()][(int) personaje.getPosicionY()] = personaje;
 	}
 }
